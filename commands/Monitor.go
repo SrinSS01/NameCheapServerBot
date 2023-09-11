262bacd4abb5082b8161522b22f3c0e326237ff3
SrinSS01 <srinjoy.s.01.in@gmail.com>
2023-09-11 23:13:17 +0530

    no comment

--- commands/Monitor.go
+++ commands/Monitor.go
@@ -21,11 +21,11 @@ type MonitorCommand struct {
 
 type MonitorTaskScheduler struct {
 	MessageCount int
-	Task         func()
+	Task         func(msgOld *discordgo.Message, session *discordgo.Session)
 	StopTask     chan bool
 }
 
-var monitor map[string]MonitorTaskScheduler
+var monitor = map[string]*MonitorTaskScheduler{}
 
 var Monitor = MonitorCommand{
 	Command: &discordgo.ApplicationCommand{
@@ -33,10 +33,30 @@ var Monitor = MonitorCommand{
 		Description: "tracking information for the messages in the account's message queue",
 		Options: []*discordgo.ApplicationCommandOption{
 			{
-				Type:        discordgo.ApplicationCommandOptionString,
-				Name:        "email",
-				Description: "Email to monitor",
-				Required:    true,
+				Type:        discordgo.ApplicationCommandOptionSubCommand,
+				Name:        "start",
+				Description: "start monitoring",
+				Options: []*discordgo.ApplicationCommandOption{
+					{
+						Type:        discordgo.ApplicationCommandOptionString,
+						Name:        "email",
+						Description: "Email to start monitor",
+						Required:    true,
+					},
+				},
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionSubCommand,
+				Name:        "stop",
+				Description: "Stop monitoring",
+				Options: []*discordgo.ApplicationCommandOption{
+					{
+						Type:        discordgo.ApplicationCommandOptionString,
+						Name:        "email",
+						Description: "Email to stop monitor",
+						Required:    true,
+					},
+				},
 			},
 		},
 	},
@@ -84,117 +104,104 @@ func (m *MonitorCommand) Execute(session *discordgo.Session, interaction *discor
 		Type: discordgo.InteractionResponseDeferredChannelMessageWithSource,
 	})
 	commandData := interaction.ApplicationCommandData()
-	email := commandData.Options[0].StringValue()
-	cPanelUserName := m.Config.BasicAuth.Username
-	cPanelPassword := m.Config.BasicAuth.Password
-	apiUrl := "https://wch-llc.com:2083/json-api/cpanel?cpanel_jsonapi_user=user&cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=EmailTrack&cpanel_jsonapi_func=search&success=1&defer=0&recepient=" + url.QueryEscape(email)
-	content := "Starting to monitor"
-	msg, _ := session.InteractionResponseEdit(interaction.Interaction, &discordgo.WebhookEdit{
-		Content: &content,
-	})
-	client := &http.Client{}
-	result, err := RequestEmailTrack(client, apiUrl, cPanelUserName, cPanelPassword)
-	if err != nil {
-		_, _ = session.ChannelMessageSendReply(msg.ChannelID, err.Error(), msg.Reference())
-		return
-	}
-	stop := make(chan bool)
+	subCommand := commandData.Options[0]
+	email := subCommand.Options[0].StringValue()
+	switch subCommand.Name {
+	case "stop":
+		monitorTaskScheduler := monitor[email]
+		if monitorTaskScheduler == nil {
+			content := "email not monitored yet"
+			_, _ = session.InteractionResponseEdit(interaction.Interaction, &discordgo.WebhookEdit{
+				Content: &content,
+			})
+			return
+		}
+		content := "Stopped monitoring " + email
+		_, _ = session.InteractionResponseEdit(interaction.Interaction, &discordgo.WebhookEdit{
+			Content: &content,
+		})
+		monitorTaskScheduler.StopTask <- true
+		break
+	case "start":
+		cPanelUserName := m.Config.BasicAuth.Username
+		cPanelPassword := m.Config.BasicAuth.Password
+		apiUrl := "https://wch-llc.com:2083/json-api/cpanel?cpanel_jsonapi_user=user&cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=EmailTrack&cpanel_jsonapi_func=search&success=1&defer=0&recipient=" + url.QueryEscape(email)
+		content := "Starting to monitor"
+		msg, _ := session.InteractionResponseEdit(interaction.Interaction, &discordgo.WebhookEdit{
+			Content: &content,
+		})
+		client := &http.Client{}
+		result, err := RequestEmailTrack(client, apiUrl, cPanelUserName, cPanelPassword)
+		if err != nil {
+			_, _ = session.ChannelMessageSendReply(msg.ChannelID, err.Error(), msg.Reference())
+			return
+		}
+		stop := make(chan bool)
 
-	ticker := time.NewTicker(time.Second * 5)
+		ticker := time.NewTicker(time.Second * 5)
 
-	task := func() {
-		msgOld := msg
-		for {
-			select {
-			case <-stop:
-				ticker.Stop()
-				return
-			case <-ticker.C:
-				req, err := RequestEmailTrack(client, apiUrl, cPanelUserName, cPanelPassword)
-				if err != nil {
+		task := func(msgOld *discordgo.Message, session *discordgo.Session) {
+			for {
+				select {
+				case <-stop:
+					ticker.Stop()
 					return
-				}
-				mts := monitor[email]
-				if datas, ok := req.CPanelResult.Data.([]interface{}); ok {
-					newMsgCount := len(datas)
-					if mts.MessageCount < newMsgCount {
-						data := datas[newMsgCount-1]
-						if dataObj, ok := data.(map[string]interface{}); ok {
-							sender := dataObj["sender"].(string)
-							recipient := dataObj["recipient"].(string)
-							senderip := dataObj["senderip"].(string)
-							senderhost := dataObj["senderhost"].(string)
-							actionunixtime := dataObj["actionunixtime"].(string)
-							msgOld, _ = session.ChannelMessageSendEmbedReply(msgOld.ChannelID, &discordgo.MessageEmbed{
-								Title: "Email Recieved!",
-								Fields: []*discordgo.MessageEmbedField{
-									{
-										Name:  "To",
-										Value: fmt.Sprintf("`%s`", recipient),
+				case <-ticker.C:
+					req, err := RequestEmailTrack(client, apiUrl, cPanelUserName, cPanelPassword)
+					if err != nil {
+						return
+					}
+					mts := monitor[email]
+					if datas, ok := req.CPanelResult.Data.([]interface{}); ok {
+						newMsgCount := len(datas)
+						if mts.MessageCount < newMsgCount {
+							mts.MessageCount = newMsgCount
+							data := datas[0]
+							if dataObj, ok := data.(map[string]interface{}); ok {
+								sender := dataObj["email"].(string)
+								recipient := dataObj["recipient"].(string)
+								senderip := dataObj["senderip"].(string)
+								senderhost := dataObj["senderhost"].(string)
+								actionunixtime := dataObj["actionunixtime"].(float64)
+								msgOld, _ = session.ChannelMessageSendEmbedReply(msgOld.ChannelID, &discordgo.MessageEmbed{
+									Title: "Email Recieved!",
+									Color: 0x00FF00,
+									Fields: []*discordgo.MessageEmbedField{
+										{
+											Name:  "To",
+											Value: fmt.Sprintf("`%s`", recipient),
+										},
+										{
+											Name:  "From",
+											Value: fmt.Sprintf("`%s`", sender),
+										},
+										{
+											Name:  "Sender IP",
+											Value: fmt.Sprintf("`%s`", senderip),
+										},
+										{
+											Name:  "Sender Host",
+											Value: fmt.Sprintf("`%s`", senderhost),
+										},
+										{
+											Name:  "Action Time",
+											Value: fmt.Sprintf("<t:%d>", int64(actionunixtime)),
+										},
 									},
-									{
-										Name:  "From",
-										Value: fmt.Sprintf("`%s`", sender),
-									},
-									{
-										Name:  "Sender IP",
-										Value: fmt.Sprintf("`%s`", senderip),
-									},
-									{
-										Name:  "Sender Host",
-										Value: fmt.Sprintf("`%s`", senderhost),
-									},
-									{
-										Name:  "Action Time",
-										Value: fmt.Sprintf("<t:%s>", actionunixtime),
-									},
-								},
-							}, msgOld.MessageReference)
+								}, msgOld.Reference())
+							}
 						}
 					}
 				}
 			}
 		}
-	}
-	if datas, ok := result.CPanelResult.Data.([]interface{}); ok {
-		monitor[email] = MonitorTaskScheduler{
-			MessageCount: len(datas),
-			Task:         task,
-			StopTask:     stop,
+		if datas, ok := result.CPanelResult.Data.([]interface{}); ok {
+			monitor[email] = &MonitorTaskScheduler{
+				MessageCount: len(datas),
+				Task:         task,
+				StopTask:     stop,
+			}
 		}
+		task(msg, session)
 	}
-
-	/*
-	   	// You can edit this code!
-	      // Click here and start typing.
-	      package main
-
-	      import (
-	      	"fmt"
-	      	"os"
-	      	"os/signal"
-	      	"syscall"
-	      	"time"
-	      )
-
-	      func main() {
-	      	task(time.Now())
-	      	tick := time.NewTicker(time.Second * 5)
-	      	scheduler(tick)
-	      	sigs := make(chan os.Signal, 1)
-	      	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	      	<-sigs
-	      	tick.Stop()
-	      }
-
-	      func scheduler(tick *time.Ticker) {
-	      	for t := range tick.C {
-	      		task(t)
-	      	}
-	      }
-
-	      func task(t time.Time) {
-	      	fmt.Println("hello! printed at ", t)
-	      }
-	*/
 }
